9fc21b1956596edcd14af99a7cab40fb851b0ca2
ll708 <l84083@gmail.com>
2025-11-24 16:29:49 +0800

    新增 Maven 封裝及 Spring Boot 應用程式架構，包含公告 API 和資料庫操作層

---
+++ otbackend/src/test/java/com/example/openticket/OpenticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
